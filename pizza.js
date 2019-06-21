eb62b52adee9478f6e62dcaf87909b3cee044693
Fankadore <rmackenzie88@gmail.com>
2019-06-21 17:45:23 +0100

    .

--- pizza.js
+++ pizza.js
@@ -6,16 +6,17 @@ const inputQuantity1 = document.querySelector('#input-quantity1');
 const inputQuantity2 = document.querySelector('#input-quantity2');
 const inputCost1 = document.querySelector('#input-cost1');
 const inputCost2 = document.querySelector('#input-cost2');
-const textboxOutput = document.querySelector('#textbox-output');
+const textboxOutput1 = document.querySelector('#textbox-output1');
+const textboxOutput2 = document.querySelector('#textbox-output2');
 const buttonCalc = document.querySelector('#button-calc');
 
 function calculatePizza() {
-  const size1 = inputSize1.value || 18;
-  const size2 = inputSize2.value || 9;
-  const quantity1 = inputQuantity1.value || 1;
-  const quantity2 = inputQuantity2.value || 2;
-  const cost1 = inputCost1.value || 15.00;
-  const cost2 = inputCost2.value || 5.00;
+  const size1 = +inputSize1.value || 18;
+  const size2 = +inputSize2.value || 9;
+  const quantity1 = +inputQuantity1.value || 1;
+  const quantity2 = +inputQuantity2.value || 2;
+  const cost1 = +inputCost1.value || 15.00;
+  const cost2 = +inputCost2.value || 5.00;
 
   const area1 = Math.PI * ((size1 / 2) ** 2);
   const area2 = Math.PI * ((size2 / 2) ** 2);
@@ -29,8 +30,13 @@ function calculatePizza() {
   const costPerSquareInch1 = (cost1 / quantity1) / area1;
   const costPerSquareInch2 = (cost2 / quantity2) / area2;
 
-  textboxOutput.innerHTML = `${quantity1} x ${size1}" pizza = ${+areaTotal1.toFixed(2)}in² or ${+ratio1.toFixed(2)} x ${size2}" pizzas at £${costPerSquareInch1.toFixed(2)} per square inch<br>`;
-  textboxOutput.innerHTML += `${quantity2} x ${size2}" pizza = ${+areaTotal2.toFixed(2)}in² or ${+ratio2.toFixed(2)} x ${size1}" pizzas at £${costPerSquareInch2.toFixed(2)} per square inch`;
+  textboxOutput1.innerHTML = `<u>Deal 1 Results</u><br>`;
+  textboxOutput1.innerHTML += `${quantity1} x ${size1}" pizza = ${+ratio1.toFixed(2)} x ${size2}" pizzas<br>`;
+  textboxOutput1.innerHTML += `£${cost1.toFixed(2)} / ${+areaTotal1.toFixed(2)}in² = £${costPerSquareInch1.toFixed(2)} per square inch`;
+  
+  textboxOutput2.innerHTML = `<u>Deal 2 Results</u><br>`;
+  textboxOutput2.innerHTML += `${quantity2} x ${size2}" pizza = ${+ratio2.toFixed(2)} x ${size1}" pizzas<br>`;
+  textboxOutput2.innerHTML += `£${cost2.toFixed(2)} / ${+areaTotal2.toFixed(2)}in² = £${costPerSquareInch2.toFixed(2)} per square inch`;
 }
 
 buttonCalc.addEventListener('click', calculatePizza);
